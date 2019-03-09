0d110e54a909af8d4822b094800bf390e4b21ed5
Zachary Kirby <i.am.kirbynator@gmail.com>
2019-03-09 14:24:33 -0700

    added color

--- db/migrate/20190309174737_add_first_name_and_last_name_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFirstNameAndLastNameToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :fistName, :string, null: false
-    add_column :users, :lastName, :string, null: false
-  end
-end
