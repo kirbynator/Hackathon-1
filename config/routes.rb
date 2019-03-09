cb3cdfb492b2cb62114794a80cdbd4d653809165
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-03-09 10:29:34 -0700

    commiting

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  get 'locations/index'
-  get 'locations/show'
-  get 'locations/new'
-  get 'locations/edit'
-  get 'addresses/index'
-  get 'addresses/show'
-  get 'addresses/new'
-  get 'addresses/edit'
-  get 'trips/index'
-  get 'trips/show'
-  get 'trips/new'
-  get 'trips/edit'
+root 'trip#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
