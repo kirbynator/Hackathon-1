9092c83aa39598e6bd8b94d2778ec4c715f78072
Zachary Kirby <i.am.kirbynator@gmail.com>
2019-03-09 10:17:21 -0700

    fisrt edition of controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'locations/index'
-  get 'locations/show'
-  get 'locations/new'
-  get 'locations/edit'
-  get 'addresses/index'
-  get 'addresses/show'
-  get 'addresses/new'
-  get 'addresses/edit'
-  get 'trips/index'
-  get 'trips/show'
-  get 'trips/new'
-  get 'trips/edit'
+ root "trips#index"
+ resourses :users do
+resourses :trips
+ end
+  resourses :trips do
+respourse :locations
+ end
+ resourses :locations do
+  resourses :address
+ end
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
